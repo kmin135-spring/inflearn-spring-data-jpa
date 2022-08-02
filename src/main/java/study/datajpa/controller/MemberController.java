dff03fc7a6bc4eff1b27b1d1ee56c6877e8900bf
khs <kmin135@naver.com>
2022-08-02 20:17:42 +0900

    web - 페이징과 정렬

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -1,9 +1,13 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -32,6 +36,37 @@ public class MemberController {
 
     @PostConstruct
     public void init() {
-        memberRepo.save(Member.of("testuser", 25));
+        for(int i=0;i<100;i++) {
+            memberRepo.save(Member.of("testuser" + i, 10+i));
+        }
+    }
+
+    /**
+     * 기본 페이징 & 정렬 제공
+     * ex1) members?page=1&size=3
+     * ex2) members?page=1&size=3&sort=id,desc&sort=username,desc
+     */
+    @GetMapping("/members")
+    public Page<Member> list(Pageable pageable) {
+        System.out.println(pageable.getClass());
+        return memberRepo.findAll(pageable);
+    }
+
+    /**
+     * 실무에서는 entity를 그대로 리턴하지마라
+     * dto로 변환해서 리턴
+     */
+    @GetMapping("/members-dto")
+    public Page<MemberDto> listDto(Pageable pageable) {
+        return memberRepo.findAll(pageable).map(m -> new MemberDto(m.getId(), m.getUsername(), null));
+    }
+
+    /**
+     * yml의 글로벌 설정보다 @PageableDefault 로 요청별로 기본값을 설정할 수 있음
+     */
+    @GetMapping("/customer-members")
+    public Page<Member> customList(@PageableDefault(size = 5, sort = "username") Pageable pageable) {
+        System.out.println(pageable.getClass());
+        return memberRepo.findAll(pageable);
     }
 }
--- src/main/java/study/datajpa/dto/MemberDto.java
+++ src/main/java/study/datajpa/dto/MemberDto.java
@@ -2,11 +2,26 @@ package study.datajpa.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import study.datajpa.entity.Member;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class MemberDto {
     private Long id;
     private String username;
     private String teamName;
+
+    /**
+     * DTO는 엔티티의 세부구현을 알아도 된다.
+     * DTO에 엔티티를 파라미터로 받는 정적 메서드 팩토리 구현
+     * */
+    public static MemberDto of(Member member) {
+        MemberDto md = new MemberDto();
+        md.setId(member.getId());
+        md.setUsername(member.getUsername());
+        md.setTeamName(null);
+        return md;
+    }
 }
